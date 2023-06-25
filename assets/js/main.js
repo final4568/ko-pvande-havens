3b67bdcb5f8d38904f9c6bfae8157da837303cb2
Usama Ali <final.project.gmc@gmail.com>
2023-06-25 16:43:29 +0500

    woking on blog single

--- assets/js/main.js
+++ assets/js/main.js
@@ -198,6 +198,135 @@
 			},
 		  },
 	  });
+	  function kovp_pagination() {
 
+		// target container
+		let container = '.facetwp-pager';
+
+		// exist if facetwp not on the page
+		if (!$(container).length) {
+			return;
+		}
+
+		// range limit for mobile
+		let mb_limit = 0;
+
+		// end point range limit for mobile
+		let mb_endpoints_limit = 1;
+
+		// range limit for desktop
+		let dk_limit = 1;
+
+		// end point range limit for desktop
+		let dk_endpoints_limit = 2;
+
+		let mb_hide_class = 'kovp-hide-page-mb';
+		let dk_hide_class = 'kovp-hide-page-dk';
+
+		// dots markup
+		let dots_start = '<a class="facetwp-page dots start ' + mb_hide_class + ' ' + dk_hide_class + '">…</a>';
+		let dots_end = '<a class="facetwp-page dots end ' + mb_hide_class + ' ' + dk_hide_class + '">…</a>';
+
+		// ----------------------------------------------------
+
+		let total_pages = parseInt($(container + ' a.facetwp-page.last').attr('data-page'));
+		let active_page = parseInt($(container + ' a.facetwp-page.active').attr('data-page'));
+
+		// check if the current page is an endpoint
+		let is_endpoint_page = (active_page === 1 || active_page === total_pages) ? true : false;
+
+		let mb_range_start = is_endpoint_page ? active_page - mb_endpoints_limit : active_page - mb_limit;
+		let mb_range_end = is_endpoint_page ? active_page + mb_endpoints_limit : active_page + mb_limit;
+
+		let dk_range_start = is_endpoint_page ? active_page - dk_endpoints_limit : active_page - dk_limit;
+		let dk_range_end = is_endpoint_page ? active_page + dk_endpoints_limit : active_page + dk_limit;
+
+		let dots_mb_start, dots_mb_end, dots_dk_start, dots_dk_end = false;
+
+		// ----------------------------------------------------
+
+		$(container + ' .dots').remove();
+
+		$(container + ' a.facetwp-page:not(.prev, .next)').removeClass(mb_hide_class + ' ' + dk_hide_class);
+
+		// loop over all the page numbers, excluding 'previous' and 'next' elements
+		$(container + ' a.facetwp-page:not(.active, .prev, .next, [data-page="1"], [data-page="' + total_pages + '"])').each(function (i, obj) {
+
+			let current_page = $(this).attr('data-page');
+
+			if (current_page < mb_range_start) {
+				$(this).addClass(mb_hide_class);
+				dots_mb_start = true;
+			}
+
+			if (current_page > mb_range_end) {
+				$(this).addClass(mb_hide_class);
+				dots_mb_end = true;
+			}
+
+			if (current_page < dk_range_start) {
+				$(this).addClass(dk_hide_class);
+				dots_dk_start = true;
+			}
+
+			if (current_page > dk_range_end) {
+				$(this).addClass(dk_hide_class);
+				dots_dk_end = true;
+			}
+
+		});
+
+		// add dots element - start
+		if (dots_mb_start || dots_dk_start) {
+
+			$(container + ' a.facetwp-page[data-page="1"]').after(dots_start);
+
+			if (dots_mb_start) {
+				$(container + ' .dots.start').removeClass(mb_hide_class);
+			}
+
+			if (dots_dk_start) {
+				$(container + ' .dots.start').removeClass(dk_hide_class);
+			}
+
+		}
+
+		// add dots element - end
+		if (dots_mb_end || dots_dk_end) {
+
+			$(container + ' a.facetwp-page[data-page="' + total_pages + '"]').before(dots_end);
+
+			if (dots_mb_end) {
+				$(container + ' .dots.end').removeClass(mb_hide_class);
+			}
+
+			if (dots_dk_end) {
+				$(container + ' .dots.end').removeClass(dk_hide_class);
+			}
+
+		}
+
+		active_page = active_page ? active_page : 1;
+		total_pages = total_pages ? total_pages : 1;
+
+		$('.kovp-total-page-counter span.kovp-current-page').html(active_page);
+		$('.kovp-total-page-counter span.kovp-total-page').html(total_pages);
+
+	}
+
+	  $(window).on('load resize', function () {
+		kovp_pagination();
+	});
+	  document.addEventListener('facetwp-loaded', function () {
+		if (FWP.loaded) {
+			$('.facetwp-facet-post_pagination').show();
+			kovp_pagination();
+			if ($('.facetwp-scroll-top').length) {
+				$('html,body').animate({
+					scrollTop: $('.facetwp-scroll-top').offset().top - $('.elementor-location-header').height()
+				});
+			}
+		}
+	});
 }(jQuery))
 
